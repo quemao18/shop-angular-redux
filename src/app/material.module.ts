c8f9784a4dd093e44d72599d59241bb8a0663139
Alejandro@BEELZEBUB <alejandro@evotechnology.co>
2020-03-26 20:53:02 -0400

    fix

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,16 +4,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { NavComponent } from './nav/nav.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { LayoutModule } from '@angular/cdk/layout';
 
-
-
 @NgModule({
-  declarations: [
-    
-  ],
   exports: [
     CommonModule,
     MatToolbarModule,
